e9eb7a133c0226143efc731eb206781e86da34ad
Sukhmai Kapur <39801345+Sukhmai@users.noreply.github.com>
2018-09-10 00:22:18 -0400

    The extension now saves words to storage.
    I fixed background.js so it now saves words to storage when you ask it to. I also created template files for the website function of the extension.

--- background.js
+++ background.js
@@ -1,13 +1,21 @@
 function translateWord(info,tab) {
     console.log("You tried to save: " + info.selectionText);
-    chrome.tabs.create({
-    url: "http://www.google.com/search?q=" + info.selectionText
-  });
+    chrome.storage.sync.get('words', function(data) {
+        var newWords=data.words;
+        newWords.push(info.selectionText);
+        chrome.storage.sync.set({'words':newWords}, function(){
+            chrome.storage.sync.get('words', function(data) {
+                console.log("You saved: " + data.words[data.words.length-1]);
+            });
+        });
+    });
 }
-chrome.contextMenus.create({
-    id:"transSave",
-    title: "Translate and Save: %s",
-    contexts:["selection"],
-    //onclick: translateWord
+
+chrome.storage.sync.set({'words':[]}, function() {
+    chrome.contextMenus.create({
+        id:"transSave",
+        title: "Translate and Save: %s",
+        contexts:["selection"],
+    });
+    chrome.contextMenus.onClicked.addListener(translateWord);
 });
-chrome.contextMenus.onClicked.addListener(translateWord);

69a2262367b18a171933f49615a26136753f6b32
Sukhmai Kapur <39801345+Sukhmai@users.noreply.github.com>
2018-09-15 15:56:59 -0400

    This added requests to the translate api
    I added a request to the glosbe api, and I store the the translated word in a translated array.

--- background.js
+++ background.js
@@ -1,21 +1,43 @@
 function translateWord(info,tab) {
-    console.log("You tried to save: " + info.selectionText);
-    chrome.storage.sync.get('words', function(data) {
-        var newWords=data.words;
-        newWords.push(info.selectionText);
-        chrome.storage.sync.set({'words':newWords}, function(){
-            chrome.storage.sync.get('words', function(data) {
-                console.log("You saved: " + data.words[data.words.length-1]);
+    var inputWord = info.selectionText;
+    var inputUrl = "https://glosbe.com/gapi/translate?from=spa&dest=eng&format=json&phrase="+inputWord;
+    var request = new XMLHttpRequest;
+    request.open('GET', inputUrl, true);
+    request.responseType = 'text';
+    request.send();
+    request.onload = function() {
+        var text = request.response;
+        var parsed = JSON.parse(text);
+        var translatedWord = parsed.tuc[0].phrase.text;
+        console.log("You tried to save: " + inputWord + " : " + translatedWord);
+        chrome.storage.sync.get('words', function(data) {
+            chrome.storage.sync.get('translated', function(data2) {
+                var newTranslated = data2.translated;
+                newTranslated.push(translatedWord);
+                chrome.storage.sync.set({'translated':newTranslated}, function(){
+                    chrome.storage.sync.get('translated', function(data3) {
+                        console.log("You saved: " + data3.translated[data3.translated.length-1]);
+                    });
+                });
+            });
+            var newWords = data.words;
+            newWords.push(inputWord);
+            chrome.storage.sync.set({'words':newWords}, function(){
+                chrome.storage.sync.get('words', function(data4) {
+                    console.log("You saved: " + data4.words[data4.words.length-1]);
+                });
             });
         });
-    });
+    };
 }
 
 chrome.storage.sync.set({'words':[]}, function() {
-    chrome.contextMenus.create({
-        id:"transSave",
-        title: "Translate and Save: %s",
-        contexts:["selection"],
+    chrome.storage.sync.set({'translated':[]}, function() {
+        chrome.contextMenus.create({
+            id:"transSave",
+            title: "Translate and Save: %s",
+            contexts:["selection"],
+        });
+        chrome.contextMenus.onClicked.addListener(translateWord);
     });
-    chrome.contextMenus.onClicked.addListener(translateWord);
 });
